0660c4b10f1400b19302e0b45c6c8848ea592770
Sollano R Braga <sollanorb@gmail.com>
2017-08-29 01:03:47 -0300

    check for dap < 1.3

--- funs/consistency.R
+++ funs/consistency.R
@@ -52,13 +52,31 @@ consistency <- function(df, dap, ht, parcela,lower=0.2, upper=10){
     summarise_at(vars(DAPname, HTname), funs(mean(.,na.rm=T), sd(.,na.rm=T) ) ) %>% 
     full_join(DF,by=PARCC) %>% 
     mutate(
-      DAP_test = ifelse(.data[[DAPname]] <=  .data[[paste(DAPname,"mean",sep="_") ]] -.data[[paste(DAPname,"sd",sep="_") ]]*3, "DAP menor que media - 3 sd", ifelse(.data[[DAPname]] >  .data[[paste(DAPname,"mean",sep="_") ]] + .data[[paste(DAPname,"sd",sep="_") ]]*3, "DAP maior que media + 3 sd", "ok" )  ),
+      DAP_test = case_when(
+        .data[[DAPname]] <= .data[[paste(DAPname,"mean",sep="_") ]] - .data[[paste(DAPname,"sd",sep="_") ]]*3 ~ "DAP menor que media - 3 sd",
+        .data[[DAPname]] >  .data[[paste(DAPname,"mean",sep="_") ]] + .data[[paste(DAPname,"sd",sep="_") ]]*3 ~ "DAP maior que media + 3 sd",
+        !is.na( .data[[DAPname]] ) & .data[[DAPname]] < 1.3                                                   ~ "Dap menor que 1,3",
+        TRUE                                                                                                  ~ "ok"
+      ),
+      
       HT_test = ifelse(.data[[HTname]]  <=  .data[[paste(HTname,"mean",sep="_") ]] - .data[[paste(HTname,"sd",sep="_") ]]*3, "Altura menor que media - 3 sd", ifelse(.data[[HTname]]  >  .data[[paste(HTname,"mean",sep="_") ]] + .data[[paste(HTname,"sd",sep="_") ]]*3, "Altura maior que media + 3 sd", ifelse(is.na( .data[[DAPname]] ) & is.numeric( .data[[HTname]] ), "Arvore com altura e sem dap", "ok")       )  ),
-      ratio_test =  ifelse(.data[[DAPname]]/.data[[HTname]] <= lower | .data[[DAPname]]/.data[[HTname]] >= upper , "Razao dap/ht ruim", "ok"  )  
+      
+      HT_test = case_when(
+        .data[[HTname]]  <=  .data[[paste(HTname,"mean",sep="_") ]] - .data[[paste(HTname,"sd",sep="_") ]]*3 ~ "Altura menor que media - 3 sd",
+        .data[[HTname]]  >  .data[[paste(HTname,"mean",sep="_") ]] + .data[[paste(HTname,"sd",sep="_") ]]*3  ~ "Altura maior que media + 3 sd",
+        is.na( .data[[DAPname]] ) & is.numeric( .data[[HTname]] )                                            ~ "Arvore com altura e sem dap",
+        TRUE                                                                                                 ~ "ok"
+      ),
+      
+      ratio_test = case_when(
+        .data[[DAPname]]/.data[[HTname]] <= lower | .data[[DAPname]]/.data[[HTname]] >= upper ~ "Razao dap/ht ruim",
+        TRUE                                                                                  ~ "ok"  
+        
+      )
     ) %>% 
-    filter( !is.na(.data[[DAPname]]) & !is.na(.data[[HTname]])  ) %>% 
+    filter( !is.na(.data[[DAPname]]) | !is.na(.data[[HTname]])  ) %>% 
     filter(DAP_test != "ok" | HT_test != "ok" | ratio_test != "ok") %>% 
-   # filter_at(vars( "DAP_test", "HT_test", "ratio_test" ), any_vars(. != "ok") ) %>% 
+    # filter_at(vars( "DAP_test", "HT_test", "ratio_test" ), any_vars(. != "ok") ) %>% 
     select(rowid, DAP_test, HT_test, ratio_test,  everything() ) %>% 
     arrange(rowid) %>% 
     as.data.frame
