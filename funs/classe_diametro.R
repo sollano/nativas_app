129d7ddc7f57aecc8162fded8c9e0131c23b5394
Sollano R Braga <sollanorb@gmail.com>
2017-08-30 11:17:25 -0300

    classe de diametro updated

--- funs/classe_diametro.R
+++ funs/classe_diametro.R
@@ -12,16 +12,6 @@ classe_diametro <- function(df, dap, parcela, area_parcela, ic = 5, dapmin = 5, 
     stop("dap not set", call. = F) 
   }
   
-  # se parcela nao for fornecido ou for igual "", parar
-  if(  missing(parcela) || gsub('"', "", deparse(substitute(parcela)))== "" ){ 
-    stop("parcela not set", call. = F) 
-  }
-  
-  # se area_parcela nao for fornecido ou for igual "", parar
-  if(  missing(area_parcela) || gsub('"', "", deparse(substitute(area_parcela)))== "" ){ 
-    stop("area_parcela not set", call. = F) 
-  }
-  
   # ic precisa ser numerico e de tamanho 1
   if(!is.numeric(ic) || length(ic)>1){
     stop("ic must be a single number", call. = F)
@@ -32,23 +22,37 @@ classe_diametro <- function(df, dap, parcela, area_parcela, ic = 5, dapmin = 5, 
     stop("dapmin must be a single number", call. = F)
   }
   
+  
+  # se parcela nao for fornecida, for NULL, NA ou "", transformar em 1
+  if(missing(parcela) || is.null(parcela) || is.na(parcela) ||  parcela==""){
+    parcela <- 1
+  }
+  
+  # se area_parcela nao for fornecida, for NULL, NA ou "", transformar em 10000
+  if(missing(area_parcela) || is.null(area_parcela) || is.na(area_parcela) ||  area_parcela==""){
+    area_parcela <- 10000
+  }
+  
   # app: Se especies for "" ou NULL, transformar em NA
   if(is.null(especies) || is.na(especies) ||  especies==""){
     especies <- NA
   }
-
+  
   # app: Se volume for "" ou NULL, transformar em NA
   if(is.null(volume)|| is.na(volume) || volume==""){
     volume <- NA
   }
-
+  
+  # Parar se o usuario pedir o resultado por ha sem fornecer as variaveis necessarias
+  if(cctc_ha==T & cc_to_column==T &  parcela == 1 & area_parcela == 10000 ){stop("Parcela and area_parcela must be provided if cctc_ha=TRUE",call. = F)}
+  
   # Permitir se e nse como entrada de nomes de variaveis
   DAPAA <- dap
   PARCELAAA <- parcela
   AREA_PARCELAAA <- area_parcela
   ESPECIESAA <- especies
   VOLUMEAA <- volume
-
+  
   # Testar se PARCELA e um nome de variavel
   test <- try(df %>% pull(PARCELAAA), silent = T)
   
@@ -79,12 +83,12 @@ classe_diametro <- function(df, dap, parcela, area_parcela, ic = 5, dapmin = 5, 
   if(is(testesp, "try-error") && !is.na(especies) ){
     
     stop("especies must be a variable name",call. = F)
-  
-  # Se nao for, sera criada uma variavel descartavel no dataframe, para que o group_by possa rodar
+    
+    # Se nao for, sera criada uma variavel descartavel no dataframe, para que o group_by possa rodar
   }else if(is.na(especies) || is.null(especies) || especies==""){
     
     df$ESPECIESAA <- "yay"
-
+    
     # Se a especie for fornecida e for um nome de variavel,
     # remover as especies nao identificadas do dado
   }else if( !is(testesp, "try-error") && !is.na(especies) ){
@@ -94,7 +98,7 @@ classe_diametro <- function(df, dap, parcela, area_parcela, ic = 5, dapmin = 5, 
     
     # Remover especies nao identificadas
     df <- df[! df[[ESPECIESAA]] %in% rotulo.NI, ] 
-
+    
   }
   
   testvol<- try(df %>% pull(VOLUMEAA), silent = T)
@@ -112,7 +116,7 @@ classe_diametro <- function(df, dap, parcela, area_parcela, ic = 5, dapmin = 5, 
     VOLUMEAA <- "vol"
     
   }
-
+  
   # VOLUMEAA <- rlang::sym(VOLUMEAA)
   
   df_final <- df %>% 
@@ -133,14 +137,22 @@ classe_diametro <- function(df, dap, parcela, area_parcela, ic = 5, dapmin = 5, 
     filter(CC >= dapmin) %>% # Remover classes menores que o dap minimo
     ungroup %>% 
     as.data.frame
-
+  
   # Remover a variavel descartavel, caso tenha sido criada
   df_final$ESPECIESAA <- NULL
   
-  # Remover VOLUMEAA caso nao tenha fornecido pelo usuario
+  # Remover volume caso nao tenha fornecido pelo usuario
   if(is.na(volume) || is.null(volume) || volume==""){
     
     df_final$volume <- NULL
+    df_final$volume_ha <- NULL
+  }
+  
+  # se parcela ou area_parcela nao for fornecida, for NULL, NA ou "", remover variaveis extrapoladas ha
+  #if(missing(parcela) || is.null(parcela) || is.na(parcela) ||  parcela=="" || missing(area_parcela) || is.null(area_parcela) || is.na(area_parcela) ||  area_parcela=="" ){
+  if(parcela == 1 & area_parcela == 10000){
+    df_final$IndvHA <- NULL
+    df_final$G_ha <- NULL
     df_final$volume_ha <- NULL
   }
   
@@ -168,12 +180,12 @@ classe_diametro <- function(df, dap, parcela, area_parcela, ic = 5, dapmin = 5, 
       mutate(Total = rowSums(.[  ,  sapply(., is.numeric)  ]  ) ) %>% 
       as.data.frame %>% 
       round_df(4)
-
+    
     df_final[df_final==0] <- ""
     # Se o usuario quiser o centro de classe na coluna e tiver fornecido volume,
     # popular o centro de classe com o volume
   }else if(cc_to_column==T && !is(testesp, "try-error") && !is(testvol, "try-error") ){
-   
+    
     if(cctc_ha==T){df_final$VOL<- df_final$volume_ha}else if(cctc_ha==F){df_final$VOL<- df_final$volume}else(stop("cctc_ha must be TRUE or FALSE",call. = F))
     
     df_final <- df_final %>% 
@@ -191,5 +203,5 @@ classe_diametro <- function(df, dap, parcela, area_parcela, ic = 5, dapmin = 5, 
   }
   
   return(df_final)
-
+  
 }
