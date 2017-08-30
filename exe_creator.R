7d602d9a944feffe5ca400f290dec7cf96580138
Sollano R Braga <sollanorb@gmail.com>
2017-08-30 10:53:06 -0300

    add

---
+++ exe_creator.R
@@ -0,0 +1,11 @@
+#install.packages("RInno")
+devtools::install_github("ficonsulting/RInno")
+require(RInno)
+#RInno::install_inno()
+
+create_app(
+  app_name = "nativas_app", 
+  app_dir  = "C:/Users/solla/Documents/R/trabalhos_manejo/nativas_app",
+  pkgs     = c("shiny", "DT", "formattable", "readxl", "plyr","tidyr", "dplyr","tibble", " ggplot2", "lazyeval", "ggdendro","ggthemes","xlsx")
+  )
+compile_iss()
