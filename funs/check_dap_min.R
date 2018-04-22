c531c38c0472160a1a3f8dbe632f2738213c816a
sollano <sollanorb@gmail.com>
2018-04-21 23:36:27 -0300

    fix check_dap_min function

--- funs/check_dap_min.R
+++ funs/check_dap_min.R
@@ -1,12 +1,12 @@
-check_dap_min <- function(input_min,min.value,max.value ){
+check_dap_min <- function(input_min,max.value ){
   if(is.null(input_min)){
     
   }else if(is.na(input_min)){
     
   }else if(input_min == ""){
     
-  }else if( ! input_min >= min.value | ! input_min <= max.value ){
-    paste( "dap minimo deve ser maior que o menor valor de dap e menor que o maior valor de dap")
+  }else if( input_min > max.value ){
+    paste( "dap minimo deve ser menor que o maior valor de dap")
   }
   
 }
