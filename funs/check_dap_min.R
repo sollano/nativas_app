859aea85aef40fe8d8210010fdefd821c5252d9f
sollano <sollanorb@gmail.com>
2018-04-21 23:59:18 -0300

    add error messages for missing min value

--- funs/check_dap_min.R
+++ funs/check_dap_min.R
@@ -2,9 +2,9 @@ check_dap_min <- function(input_min,max.value ){
   if(is.null(input_min)){
     
   }else if(is.na(input_min)){
-    
+    paste( "Please insert a value for 'dap minimo'")
   }else if(input_min == ""){
-    
+    paste( "Please insert a value for 'dap minimo'")
   }else if( input_min > max.value ){
     paste( "dap minimo deve ser menor que o maior valor de dap")
   }
