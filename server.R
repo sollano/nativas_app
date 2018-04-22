31d0a69e712d75ce51e4865192b5ac69fd1b4bb3
sollano <sollanorb@gmail.com>
2018-04-21 23:48:12 -0300

    remove addtional argument

--- server.R
+++ server.R
@@ -733,10 +733,9 @@ shinyServer(function(input, output, session) {
     
     # Primeiro verificamos se o dap minimo iserido pelo usuario
     # nao ultrapassa os limites do dap fornecido
-    min.val <- min(data[[nm$dap]],na.rm=T)
     max.val <- max(data[[nm$dap]],na.rm=T)
     
-    validate(check_dap_min(nm$diam.min,min.val,max.val)) 
+    validate(check_dap_min(nm$diam.min,max.val)) 
     
     # caso nao ultrapasse, filtrar
     #data <- data %>% dplyr::filter((!!rlang::sym(nm$dap)) >= nm$diam.min)
