ef1592ff555d5f208d2f684593ea101d6c635d90
Sollano Rabelo Braga <sollanorb@gmail.com>
2020-01-20 22:21:26 -0300

    v num update

--- ui.R
+++ ui.R
@@ -97,7 +97,7 @@ shinyUI(
           
           
           # Version ####
-          navbarPage("App Inventário de Nativas 2.2.0",id="tab",
+          navbarPage("App Inventário de Nativas 2.2.1",id="tab",
           #         ####           
                      theme = "green_yeti2.css",
                      # theme = "green.css", # seleciona um tema contido na pasta www
